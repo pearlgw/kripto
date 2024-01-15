348103c1a358a0fc9510bb00b6aeb6dd386d535b
pearlgw <145454245+pearlgw@users.noreply.github.com>
2024-01-15 11:00:07 +0700

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -15,12 +15,16 @@ class PDFController extends Controller
         // Mengambil informasi kandidat terpilih oleh pengguna
         $selectedCandidate = $user->vote ? $user->vote->candidate : null;
         $voteTime = $user->vote->created_at;
+        $carbonObject = Carbon::parse($voteTime)->locale('id_ID');
+
+        // Format manual hari, tanggal, bulan, tahun, jam, dan menit
+        $formattedDate = $carbonObject->isoFormat('dddd, D MMMM YYYY [pukul] H:mm [WIB]');
 
         // Load view sertifikat dengan data pengguna dan kandidat terpilih
         $pdf = PDF::loadView('student.sertifikat', [
             'user' => $userName,
             'selectedCandidate' => $selectedCandidate,
-            'voteTime' => $voteTime,
+            'voteTime' => $formattedDate,
         ]);
 
 
