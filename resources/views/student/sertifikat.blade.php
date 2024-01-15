a108aa9285930be6712bde6de46061be03bfc72c
Talitha Olga Ramadhani <talitharamadhani15@gmail.com>
2024-01-15 22:40:57 +0700

    update tampilan login & dashboard mhs👩🏻‍💻

--- resources/views/student/sertifikat.blade.php
+++ resources/views/student/sertifikat.blade.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sertifikat Pemilu UDINUS</title>
     <style>
         @page {
-            size: landscape; /* Set orientasi landscape */
+            size: landscape;
+            /* Set orientasi landscape */
         }
+
         @font-face {
             font-family: 'Lemon';
             src: url('public/Lemon-Regular.ttf') format('truetype');
             /* Sesuaikan format font dan path sesuai kebutuhan */
         }
+
         body {
             font-family: 'Lemon', sans-serif;
             text-align: center;
             margin: 50px;
             transform: rotate(0deg);
             transform-origin: left top;
-            background-size: contain; /* Adjust to cover the entire page */
+            background-size: contain;
+            /* Adjust to cover the entire page */
 
         }
 
@@ -45,21 +50,27 @@
             color: #f4bc44;
             font-family: 'Lemon', sans-serif;
         }
+
         .content strong {
             text-transform: uppercase;
         }
+
         .signature {
-            position: relative; /* Gunakan relative position untuk signature */
-            float: right; /* Float ke kanan untuk membawa ke pojok kanan */
-            margin-top: 30px; /* Sesuaikan dengan jarak dari atas */
-            margin-right: 20px; /* Sesuaikan dengan jarak dari kanan */
+            position: relative;
+            /* Gunakan relative position untuk signature */
+            float: right;
+            /* Float ke kanan untuk membawa ke pojok kanan */
+            margin-top: 30px;
+            /* Sesuaikan dengan jarak dari atas */
+            margin-right: 20px;
+            /* Sesuaikan dengan jarak dari kanan */
             font-size: 18px;
-            font-family:'Lemon', sans-serif;
+            font-family: 'Lemon', sans-serif;
             color: #f4bc44;
         }
-
     </style>
 </head>
+
 <body>
     <div class="certificate">
         <div class="header"><u>SERTIFIKAT PEMILU UDINUS</u></div><br><br>
@@ -68,14 +79,17 @@
             <strong>{{ $user }}</strong><br><br> Sebagai:<br>
             <strong>PESERTA</strong><br><br>
             Atas partisipasinya pada Pemilihan Umum DPM-KM<br>Universitas Dian Nuswantoro.<br><br>
-            @if($selectedCandidate)
-            Anda telah melakukan pemilihan pada kandidat nomor urut {{ $selectedCandidate->status }}<br>
-            pada {{ $voteTime }}<br>
+            @if ($selectedCandidate)
+                Anda telah melakukan pemilihan pada kandidat nomor urut {{ $selectedCandidate->status }}<br>
+                pada {{ $voteTime }}
             @else
-            Anda belum memilih kandidat.
+                Anda belum memilih kandidat.
             @endif
         </div>
-        <div class="signature"> Semarang, {{ \Carbon\Carbon::now()->locale('id_ID')->isoFormat('D MMMM YYYY') }}<br><br> E-Voku Udinus</div>
+        <div class="signature"> 
+            Semarang, {{ \Carbon\Carbon::now()->locale('id_ID')->isoFormat('D MMMM YYYY') }}<br><br> E-Voku Udinus
+        </div>
     </div>
 </body>
+
 </html>
